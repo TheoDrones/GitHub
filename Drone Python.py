851d456e99032ba0aa9294f4272bb69d4424b13f
vincentpadrun <vincent.padrun@gmail.com>
2020-11-30 16:44:08 +0100

    Fonction du swag

---
+++ Drone Python.py
@@ -0,0 +1,7 @@
+def fonction(name):
+    print("Hello ", name, " !")
+
+if __name__ == "__main__":
+    fonction("Théo le con")
+    fonction("Sterennou la bretonne")
+    fonction("Martinou le boss")
